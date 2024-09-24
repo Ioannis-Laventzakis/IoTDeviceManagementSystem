acc525e1e3e31f8ac61ab933c70e286bf19c8b2c
Ioannis <Glaventzakis@gmail.com>
2024-09-24 19:58:49 +0200

    first commit

---
+++ src/main/java/com/nexa/iotdevicemanagementsystem/IoTDeviceManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.nexa.iotdevicemanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IoTDeviceManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IoTDeviceManagementSystemApplication.class, args);
+	}
+
+}
