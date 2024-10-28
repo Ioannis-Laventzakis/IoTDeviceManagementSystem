8a4a3b3b23043b45e3cf9db5320c154d59533728
Ioannis <Glaventzakis@gmail.com>
2024-10-28 10:06:38 +0100

    test updated

--- src/test/java/service/UserServiceTest.java
+++ src/test/java/service/UserServiceTest.java
@@ -1,15 +1,44 @@
 package service;
 
+import dto.UserResponse;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-@WebMvcTest(UserService.class)
-@AutoConfigureMockMvc(addFilters = false)
+@SpringBootTest
 @ExtendWith(MockitoExtension.class)
 public class UserServiceTest {
+
+    @Autowired
+    private UserService userService;
+
+    @Test
+    void testGetAllUsers() {
+        UserResponse response = userService.getAllUsers();
+        assertNotNull(response);
+    }
+
+    @Test
+    void testAdd() {
+        userService.add();
+    }
+
+    @Test
+    void testDelete() {
+        userService.delete();
+    }
+
+    @Test
+    void testUpdate() {
+        userService.update();
+    }
+
+    @Test
+    void testList() {
+        userService.list();
+    }
 }
